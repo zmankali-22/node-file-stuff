ec9e2192825a2702ee7aa2681993b872d5fd6d28
acharya <sav.acharya22@gmail.com>
2024-04-26 20:03:43 +1000

    feat: async filechecker works

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const fs = require("node:fs");
+const fsPromises = require("node:fs/promises");
+
+console.log(
+  "Promise output:" + doesFileExistPromise("./pokemonStats.json")
+);
+console.log(
+  "sync output:" + doesFileExistSync("./pokemonStats.json")
+);
+
+(async () => {
+  let asyncResult = await doesFileExistAsync("./pokemonStats.json");
+  console.log("Async output:" + asyncResult.size);
+})();
+
+async function doesFileExistAsync(targetPath) {
+  return await fsPromises.stat(targetPath);
+}
+
+function doesFileExistPromise(targetPath) {
+  let result = false;
+
+  return new Promise((resolve, reject) => {
+    fsPromises
+      .stat(targetPath)
+      .then((statData) => {
+        if (statData.size || statData.birthtimeMs) {
+          result = true;
+          resolve(result);
+        } else {
+          resolve(result);
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+
+  //   fs.exists(targetpath, (existResult) => {
+  //     result = existResult;
+
+  //     return result;
+  //   });
+}
+
+function doesFileExistSync(targetpath) {
+  let result = false;
+
+  if (fs.existsSync(targetpath)) {
+    result = true;
+  } else {
+    result = false;
+  }
+
+  // Find file logic goes here
+
+  return result;
+}
+
+function createJsonFile(targetpath, data) {
+  // Todo: wishlist item because a json file already exist for us
+}
+
+function loadDataFromFile(targetpat) {
+  let data = null;
+
+  //  TODO:load data from file logic goes here
+
+  return data;
+}
+
+/*
+
+*/
+
+function saveDataToFile(targetpat, data) {
+  //  TODO:Write  data to json file logic goes here
+}
